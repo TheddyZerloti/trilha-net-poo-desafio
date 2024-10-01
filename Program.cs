0b07af99464bdadb0b3699428dbbcd3d526fc690
TheddyZerloti <theddylimazerlotti@gmail.com>
2024-10-01 16:10:04 -0400

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("SmartPhone Nokia");
+Smartphone nokia = new Nokia( numero: "6798455555", modelo: "Modelo1", imei: "asdasdad", memoria: 128);
+nokia.Ligar();
+nokia.InstalarAplicativo("TikTok");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("SmartPhone IPhone");
+Smartphone iphone = new Iphone( numero: "67986688991", modelo: "ModeloX", imei: "fghjgjjfj", memoria: 512);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("WhatsApp");
